d21dd6d11c29828b3a51c692a69f844dd9c6c9ab
Benson Osei Mensah <kbenson643@gmail.com>
2024-05-05 18:48:36 +0000

    Refactor BaseRepository to use generics
    Signed-off-by: Benson Osei Mensah <kbenson643@gmail.com>

--- repositories/base_repository.go
+++ repositories/base_repository.go
@@ -2,129 +2,93 @@ package repositories
 
 import (
 	"context"
-	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type BaseRepository struct {
+type BaseRepository[T any] struct {
 	collection *mongo.Collection
 }
 
-func NewBaseRepository(collection *mongo.Collection) *BaseRepository {
-	return &BaseRepository{collection: collection}
+func NewBaseRepository[T any](collection *mongo.Collection) *BaseRepository[T] {
+	return &BaseRepository[T]{collection: collection}
 }
 
-func (r *BaseRepository) GetCollection() *mongo.Collection {
+func (r *BaseRepository[T]) GetCollection() *mongo.Collection {
 	return r.collection
 }
 
-func (r *BaseRepository) Create(ctx context.Context, document interface{}) (interface{}, error) {
+// Create a new document
+func (r *BaseRepository[T]) Create(ctx context.Context, document T) (interface{}, error) {
 	result, err := r.collection.InsertOne(ctx, document)
-
-	return result, err
+	
+	return result.InsertedID, err
 }
 
-// FindByID finds a document by its ID
-func (r *BaseRepository) FindByID(ctx context.Context, id interface{}, result interface{}) (interface{}, error) {
+// Find a document by ID
+func (r *BaseRepository[T]) FindByID(ctx context.Context, id interface{}, result *T) (interface{}, error) {
 	err := r.collection.FindOne(ctx, id).Decode(result)
-
+	
 	return result, err
 }
 
-func (r *BaseRepository) FindAll(ctx context.Context, filter interface{}, result interface{}) (interface{}, error) {
-	cursor, err := r.collection.Find(ctx, filter)
-	if err != nil {
-		return err, nil
-	}
-	defer cursor.Close(ctx)
 
-	if err := cursor.All(ctx, result); err != nil {
-		return err, nil
+// Find all documents
+func (r *BaseRepository[T]) FindAll(ctx context.Context, filter interface{}, result *[]T) (interface{}, error) {
+	cur, err := r.collection.Find(ctx, filter)
+	if err != nil {
+		return nil, err
 	}
-	return result, nil
+	defer cur.Close(ctx)
+
+	err = cur.All(ctx, result)
+	
+	return result, err
 }
 
-// FindWithPagination finds documents with pagination
-func (r *BaseRepository) FindWithPagination(ctx context.Context, filter interface{}, page, pageSize int64) ([]interface{}, error) {
+// Find all documents with pagination
+func (r *BaseRepository[T]) FindWithPagination(ctx context.Context, filter interface{}, page, pageSize int64) ([]T, error) {
 	findOptions := options.Find()
 	findOptions.SetSkip((page - 1) * pageSize)
 	findOptions.SetLimit(pageSize)
 
-	ctx, cancel := context.WithTimeout(ctx, time.Second*5) // Add context timeout of 5 seconds
-	defer cancel()
-
-	cursor, err := r.collection.Find(ctx, filter, findOptions)
+	cur, err := r.collection.Find(ctx, filter, findOptions)
 	if err != nil {
 		return nil, err
 	}
-	defer cursor.Close(ctx)
+	defer cur.Close(ctx)
 
-	results := make([]interface{}, 0, pageSize)
-	for cursor.Next(ctx) {
-		var result interface{}
-		if err := cursor.Decode(&result); err != nil {
-			return nil, err
-		}
-		results = append(results, result)
-	}
-
-	if err := cursor.Err(); err != nil {
-		return nil, err
-	}
-
-	return results, nil
+	var results []T
+	err = cur.All(ctx, &results)
+	
+	return results, err
 }
 
-// FindWhere finds documents that match the specified filter and returns them
-func (r *BaseRepository) FindWhere(ctx context.Context, filter interface{}) ([]interface{}, error) {
-	cursor, err := r.collection.Find(ctx, filter)
-	if err != nil {
-		return nil, err
-	}
-	defer cursor.Close(ctx)
-
-	var results []interface{}
-	for cursor.Next(ctx) {
-		var result interface{}
-		if err := cursor.Decode(&result); err != nil {
-			return nil, err
-		}
-		results = append(results, result)
-	}
-
-	if err := cursor.Err(); err != nil {
-		return nil, err
-	}
-
-	return results, nil
-}
-
-// Update updates an existing document in the collection
-func (r *BaseRepository) Update(ctx context.Context, id interface{}, update interface{}) (interface{}, error) {
-	result := r.collection.FindOneAndUpdate(ctx, id, update)
-	if result.Err() != nil {
-		return nil, result.Err()
-	}
-	return result, nil
-}
-
-// UpdateMany updates multiple documents in the collection
-func (r *BaseRepository) UpdateMany(ctx context.Context, filter interface{}, update interface{}) (interface{}, error) {
-	result, err := r.collection.UpdateMany(ctx, filter, update)
+// Update a document
+func (r *BaseRepository[T]) Update(ctx context.Context, filter interface{}, update interface{}) (interface{}, error) {
+	result, err := r.collection.UpdateOne(ctx, filter, update)
+	
 	return result, err
 }
 
-// Delete deletes a document from the collection
-func (r *BaseRepository) Delete(ctx context.Context, id interface{}) error {
-	_, err := r.collection.DeleteOne(ctx, id)
-
-	return err
+// Update many documents
+func (r *BaseRepository[T]) UpdateMany(ctx context.Context, filter interface{}, update interface{}) (interface{}, error) {
+	result, err := r.collection.UpdateMany(ctx, filter, update)
+	
+	return result, err
 }
 
-// DeleteMany deletes multiple documents from the collection
-func (r *BaseRepository) DeleteMany(ctx context.Context, filter interface{}) error {
-	_, err := r.collection.DeleteMany(ctx, filter)
-	return err
+// Delete a document
+func (r *BaseRepository[T]) Delete(ctx context.Context, filter interface{}) (interface{}, error) {
+	result, err := r.collection.DeleteOne(ctx, filter)
+	
+	return result, err
+}
+
+// Delete many documents
+func (r *BaseRepository[T]) DeleteMany(ctx context.Context, filter interface{}) (interface{}, error) {
+	result, err := r.collection.DeleteMany(ctx, filter)
+	
+	return result, err
 }
